fcdb9ef58128b3f8622da5fd645df07a8832b5b2
luotao <luotaoyeah@gmail.com>
2019-02-26 19:20:43 +0800

    adopt ts-protoc-gen@0.9.0

---
+++ src/language-guide-proto3/01-defining-a-message-type/01/01_pb.d.ts
@@ -0,0 +1,25 @@
+// package: 
+// file: src/language-guide-proto3/01-defining-a-message-type/01/01.proto
+
+import * as jspb from "google-protobuf";
+
+export class Person extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Person.AsObject;
+  static toObject(includeInstance: boolean, msg: Person): Person.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Person, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Person;
+  static deserializeBinaryFromReader(message: Person, reader: jspb.BinaryReader): Person;
+}
+
+export namespace Person {
+  export type AsObject = {
+    name: string,
+  }
+}
+
